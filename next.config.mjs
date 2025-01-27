3903f37d07374d9c861bbfc796d9d61791943692
dipanja <worldofbappa@gmail.com>
2025-01-28 02:54:06 +0530

    add google image domain in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "images.unsplash.com",
-      "storage.googleapis.com/pase-achi/team-images/",
-    ],
+    domains: ["images.unsplash.com", "storage.googleapis.com"],
   },
 };
 

df675ee1eac1082e4fac1fdfd98861447e571773
dipanja <worldofbappa@gmail.com>
2025-01-28 02:54:40 +0530

    add team member data to team-member.js

---
+++ src/app/data/team-member.js
@@ -0,0 +1,65 @@
+export const teamMembers = [
+  {
+    id: 1,
+    name: "Anup Kumar Ghosh",
+    role: "Member",
+    image:
+      "https://storage.googleapis.com/pase-achi/team-images/anup-kumar-ghosh.jpeg",
+    bio: "Asstt. Teacher, Alipurduar High School (H.S.)",
+    phone: "9064647889",
+  },
+  {
+    id: 2,
+    name: "Abhidip Hazra",
+    role: "Member",
+    image:
+      "https://storage.googleapis.com/pase-achi/team-images/abhidip-hazra.jpeg",
+    bio: "Asstt. Teacher, Union Academy Kalchini",
+    phone: "7031081287",
+  },
+  {
+    id: 3,
+    name: "Arindam Basu ",
+    role: "Member",
+    image:
+      "https://storage.googleapis.com/pase-achi/team-images/arindam-basu.jpeg",
+    bio: "Asst. Teacher Punam Chand Mittal Memorial High School",
+    phone: "8617394022",
+  },
+  {
+    id: 4,
+    name: "Saikat De Bhaumik",
+    role: "Member",
+    image:
+      "https://storage.googleapis.com/pase-achi/team-images/saikat-de-bhaumik.jpeg",
+    bio: "Asstt. Teacher Sonapur B.K.High School (H.S.)",
+    phone: "9474629267",
+  },
+  {
+    id: 5,
+    name: "Avijit Baruri",
+    role: "Member",
+    image:
+      "https://storage.googleapis.com/pase-achi/team-images/avhijit-baruri.jpeg",
+    bio: "Asstt. Teacher Union Academy Kalchini",
+    phone: "",
+  },
+  {
+    id: 6,
+    name: "Ranjit Jha",
+    role: "Member",
+    image:
+      "https://storage.googleapis.com/pase-achi/team-images/ranjit-jha.jpeg",
+    bio: "Chairman, Boon English School",
+    phone: "",
+  },
+  {
+    id: 7,
+    name: "Abhishek Sarkar",
+    role: "Member",
+    image:
+      "https://storage.googleapis.com/pase-achi/team-images/abhishek-sarkar.jpeg",
+    bio: "Asstt. Teacher Hamiltonganj High School (H.S.)",
+    phone: "7001180542",
+  },
+];

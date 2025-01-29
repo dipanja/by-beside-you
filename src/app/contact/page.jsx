7850c36e83df09af70228e5b287ac9508a587e8e
dipanja <worldofbappa@gmail.com>
2025-01-30 02:00:31 +0530

    init contact and project pages

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="text-4xl p-4 text-center">
+      Contact Page under construction
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/app/projects/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <div className="text-4xl text-center p-4">
+      Projects Page under construction
+    </div>
+  );
+};
+
+export default Projects;
